f9a2c33336106e6a88d20ccdad7c59741b1c0a9d
Agyei4 <kwadwoagyei962@gmail.com>
2024-12-14 03:56:10 +0000

    basic_operation.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#initializing variables
+number1 = 10
+number2 = 5
+
+#operations = ["+","-","*"]
+#staging function
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+#displaying results
+print(f"addition of {number1} and {number2} is {addition}")
+print(f"subtraction of {number1} and {number2} is {subtraction}")
+print(f"multiplication of {number1} and {number2} is {multiplication}")
