b9629f88a58442398ea68e609b93cc8563e77aa8
Agyei4 <kwadwoagyei962@gmail.com>
2024-12-15 05:01:56 +0000

    included a rectngular area computer

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,11 @@
+# Area rectangle = length * width(breth)
+
+#initiating variables
+length = 10
+width = 5
+
+# formula to compute the area of a rectangle
+area = length * width
+
+#displaying results of the area
+print(f"The area of the rectangle is: {area}")
