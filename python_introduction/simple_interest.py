b1d3b86571317d1a46878973fe5d8318f4d45801
Agyei4 <kwadwoagyei962@gmail.com>
2024-12-15 04:52:23 +0000

    add simple interest computor

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+#stating variables for simple interest
+
+# (I = P * R * T)
+principal = 1000
+rate = 0.05
+time = 3
+
+#stating formula
+interest = principal * rate * time
+
+# displaying outcome of the simple interest
+print(f"The simple interest is: {interest}")
+#print("The simple interest is: "+ str(interest) )
