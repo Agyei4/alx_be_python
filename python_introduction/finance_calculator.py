32b90239367821d79a6520109faa12c3aed8467e
Agyei4 <kwadwoagyei962@gmail.com>
2024-12-18 03:32:32 +0000

    updated with weather advice

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,4 +1,4 @@
-
+'''
 monthly_income = float(input("Enter your monthly income: "))
 monthly_expenses = float(input("Enter your monthly expenses: "))
 
@@ -12,4 +12,14 @@ projected_savings = int((monthly_saving * 12) + (monthly_saving * 12 * 0.05))
 
 #displaying results
 print("Your monthly savings are $" + str(monthly_saving))
-print("Projected savings after one year, with interest, is: $" + str(projected_savings))
+print("Projected savings after one year, with interest, is: $" + str(projected_savings))'''
+
+value = input("Enter a value (number or string): ")
+
+match value:
+    case int():
+        print("You entered an integer:", value)
+    case str():
+        print("You entered a string:", value)
+    case _:
+        print("Invalid data type entered.")
