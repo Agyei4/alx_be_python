c71e679f21124ffb2ba849239f8c8d04fb969868
Agyei4 <kwadwoagyei962@gmail.com>
2024-12-18 09:01:17 +0000

    add a match_case_calculator script

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,20 @@
+#script to perform basic calculations using the match_case in python
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Choose the operation (+,-,*,/): ")
+
+match operation:
+    case "+":
+        print(num1 + num2) 
+    case "-":
+        print(num1 - num2)
+    case "*":
+        print(num1 * num2)
+    case "/":
+        if num2 !=0:
+            print(num1 / num2)
+        else:
+            print("Cannot divide by zero")
+    case _:
+        print("nvalid operation")
