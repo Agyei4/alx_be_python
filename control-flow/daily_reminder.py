1981108c03ef7b62978e9ed89b9017e60b87d97e
Agyei4 <kwadwoagyei962@gmail.com>
2024-12-18 10:01:54 +0000

    add a daily_reminder script

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,35 @@
+#script to acts as a daily reminder for tracking tasks
+
+#accepting inputs from user i.e. name of task, priority,if it is time bound
+name_of_task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
+
+#condtional options
+match priority:
+    case "high":
+        priority_message ="high priority task "
+    case "medium":
+        priority_message ="medium priority task "
+    case "low":
+        priority_message ="low priority task "
+    case _:
+        print("an undefined priority task ")
+
+while True:
+    if time_bound == "yes":
+        time_bound_message = "that requires immediate attention today!"
+        break
+    elif time_bound == "no":
+        time_bound_message = "that does not require immediate attention."
+        break
+    else:
+        time_bound = input("Is it time-bound? (yes/no): ").lower()
+        if time_bound == "q":
+            time_bound_message = ""
+            break
+
+dialy_message = f"{name_of_task} is a {priority_message}{time_bound_message}"
+
+print(dialy_message)
+
