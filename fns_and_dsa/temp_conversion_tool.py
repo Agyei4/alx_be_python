dd8d2c4a3c6839f2d140f07cd47cc3130651c783
Agyei4 <kwadwoagyei962@gmail.com>
2025-01-07 16:25:17 +0000

    fixed explore_datetime and temp_conversion

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -8,7 +8,7 @@ global CELSIUS_TO_FAHRENHEIT_FACTOR
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 31) * FAHRENHEIT_TO_CELSIUS_FACTOR 
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR 
     
     return celsius
 
