882d127dc0c4f6c61c6536115ffcfb55d3e0333c
Agyei4 <kwadwoagyei962@gmail.com>
2025-01-13 13:41:11 +0000

    add bank account file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,22 +1,28 @@
+from tornado.gen import Return
+
+
 class BankAccount:
-    def __init__(self, amount=0):
-        self.__amount = float(amount)
+    def __init__(self, current_balance=0):
+        self.__current_balance = float(current_balance)
 
 
     # Deposit method
     def deposit(self,amount):
-        self.__amount = self.__amount + amount
-        return self.__amount
+        self.__current_balance = self.__current_balance + amount
+        return self.__current_balance
     
     #withdraw method
     def withdraw(self,amount):
-        self.__amount = self.__amount - amount
-        return self.__amount
+        self.__current_balance = self.__current_balance - amount
+        if self.__current_balance :
+            return True
+        else:
+            return False
     
     # display_info method
     def display_balance(self):
-        print(f"Current Balance: ${self.__amount}")
-        return self.__amount
+        print(f"Current Balance: ${self.__current_balance}")
+        return self.__current_balance
 
 #
 # account1= BankAccount(9)
